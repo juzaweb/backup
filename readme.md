4a7ad694c3f14ac5a63a3d89474b860ed4e64d30
Juzaweb <dangtheanh16@gmail.com>
2024-01-21 11:48:11 +0700

    :+1: Backup config

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Backup plugin for Juzaweb CMS
--- src/Providers/BackupServiceProvider.php
+++ src/Providers/BackupServiceProvider.php
@@ -2,13 +2,37 @@
 
 namespace Juzaweb\Backup\Providers;
 
+use Illuminate\Console\Scheduling\Schedule;
+use Juzaweb\Backup\Actions\BackupAction;
 use Juzaweb\CMS\Support\ServiceProvider;
 
 class BackupServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        //
+        $this->registerHookActions([BackupAction::class]);
+
+        $this->app->booted(
+            function () {
+                $schedule = $this->app->make(Schedule::class);
+
+                if (get_config('jw_backup_enable')) {
+                    $schedule->command('backup:clean')->daily();
+                    $time = get_config('jw_backup_time', 'daily');
+
+                    switch ($time) {
+                        case 'weekly':
+                            $schedule->command('backup:run')->weekly();
+                            break;
+                        case 'monthly':
+                            $schedule->command('backup:run')->monthly();
+                            break;
+                        default:
+                            $schedule->command('backup:run')->daily();
+                    }
+                }
+            }
+        );
     }
 
     /**
